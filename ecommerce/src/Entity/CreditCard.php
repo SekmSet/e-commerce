c2427199b09af1b3dc6f3ad75d7b7163ff364bfa
Jonas <jonas.flacher@epitech.eu>
2020-07-30 16:57:37 +0200

    Entity completed and functional and ready for use fixtures with typecard, numbercard, dateexp, cvv

---
+++ ecommerce/src/Entity/CreditCard.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CreditCardRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=CreditCardRepository::class)
+ */
+class CreditCard
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="creditCards")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typecard;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $numbercard;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateexp;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cvv;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTypecard(): ?string
+    {
+        return $this->typecard;
+    }
+
+    public function setTypecard(string $typecard): self
+    {
+        $this->typecard = $typecard;
+
+        return $this;
+    }
+
+    public function getNumbercard(): ?string
+    {
+        return $this->numbercard;
+    }
+
+    public function setNumbercard(string $numbercard): self
+    {
+        $this->numbercard = $numbercard;
+
+        return $this;
+    }
+
+    public function getDateexp(): ?\DateTimeInterface
+    {
+        return $this->dateexp;
+    }
+
+    public function setDateexp(\DateTimeInterface $dateexp): self
+    {
+        $this->dateexp = $dateexp;
+
+        return $this;
+    }
+
+    public function getCvv(): ?int
+    {
+        return $this->cvv;
+    }
+
+    public function setCvv(int $cvv): self
+    {
+        $this->cvv = $cvv;
+
+        return $this;
+    }
+}

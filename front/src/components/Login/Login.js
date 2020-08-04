00bd4ad6f1d58225903b690adedd042a851adfe6
Jordan <jordan.philippot@epitech.eu>
2020-08-04 20:17:00 +0200

    Integration Register View, without swipe animation

--- front/src/components/Login/Login.js
+++ front/src/components/Login/Login.js
@@ -12,73 +12,31 @@ function Login() {
     loginUser({ username, password }).then((data) => dispatch(data));
   };
 
-
   return (
     <div className="login-view">
-
-      {/* LEFT SIDE COMPONENT */}
-      <div className="login-left">
-
-        {/* COMPONENT CENTER RECTANGLE */}
-        <div className="login-center">
-
-          {/* PINK SQUARE */}
-          <div className="side-box"></div>
-
-          {/* FORM BLOCK */}
-          <div className="login-form">
-
-            <div className="username">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-
-            <div className="password">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            <div className="submit">
-              <button className="login-btn" onClick={handleLogin}>
-                LOGIN
-              </button>
-            </div>
-
-          </div>
-          {/* END FORM */}
-
-        </div>
-        {/* END BLOCK CENTER */}
-
+      <div className="username">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
-      {/* END LEFT SIDE */}
-
-
-      {/* SWIPE BLOCK RIGHT */}
-      <div className="login-right">
-
-        {/* BUTTON SWIPE */}
-        <div className="button-swipe">
-          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-8.354 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z" />
-          </svg>
-        </div>
-
-        {/* TEXT SWIPE */}
-        <div className="text-swipe">REGISTER</div>
+      <div className="password">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
-      {/* END SWIPE BLOCK */}
-
+      <div className="submit">
+        <button className="login-btn" onClick={handleLogin}>
+          LOGIN
+        </button>
+      </div>
     </div>
   );
 }
--- front/src/components/Register/Register.js
+++ front/src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React ,{ useState } from "react";
+import React, { useState } from "react";
 import { registerUser } from "../../_actions/user_actions";
 import { useDispatch } from "react-redux";
 
@@ -13,76 +13,125 @@ function Register() {
 
   const handleRegister = (e) => {
     e.preventDefault();
-    registerUser({username, name, surname, email, password, phone}).then((data) => dispatch(data));
+    registerUser({ username, name, surname, email, password, phone }).then((data) => dispatch(data));
   };
 
   return (
-      <div className="register_view">
-        <div className="username">
-          <label htmlFor="username">Username</label>
-          <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-          />
-        </div>
-        <div className="name">
-          <label htmlFor="name">Name</label>
-          <input
-              type="text"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="surname">
-          <label htmlFor="surname">Surname</label>
-          <input
-              type="text"
-              id="surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
-              required
-          />
-        </div>
-        <div className="email">
-          <label htmlFor="email">Email</label>
-          <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-          />
-        </div>
-        <div className="password">
-          <label htmlFor="password">Password</label>
-          <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-          />
-        </div>
-        <div className="phone">
-          <label htmlFor="phone">Phone</label>
-          <input
-              type="tel"
-              id="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              required
-          />
-        </div>
-          <div className="submit">
+    <div className="register_view">
+
+      {/* LEFT SIDE COMPONENT */}
+      <div className="register-left">
+
+        {/* COMPONENT CENTER RECTANGLE */}
+        <div className="register-center">
+
+
+          {/* BLACK SQUARE */}
+          <div className="side-box-register"></div>
+
+          {/* FORM BLOCK */}
+          <div className="register-form">
+            <div className="flex-name">
+              <div className="username">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="name">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+            </div>
+
+            <div className="surname">
+              <label htmlFor="surname">Surname</label>
+              <input
+                type="text"
+                id="surname"
+                value={surname}
+                onChange={(e) => setSurname(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="password">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="phone">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="tel"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="submit">
               <button className="register-btn" onClick={handleRegister}>
-                  REGISTER
+                REGISTER
               </button>
+            </div>
+
           </div>
+          {/* END FORM */}
+
+        </div>
+        {/* END BLOCK CENTER */}
+
       </div>
+      {/* END LEFT SIDE */}
+
+
+      {/* SWIPE BLOCK RIGHT */}
+      <div className="register-right">
+
+        {/* BUTTON SWIPE */}
+        <div className="button-swipe-register">
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-8.354 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z" />
+          </svg>
+        </div>
+
+        {/* TEXT SWIPE */}
+        <div className="text-swipe-register">LOGIN</div>
+      </div>
+      {/* END SWIPE BLOCK */}
+
+
+    </div>
   );
 }
 

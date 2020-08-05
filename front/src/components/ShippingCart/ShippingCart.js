0d8b41fba05af369cb59834654501f4af2f47fcf
Beli <belinda-pro@outlook.com>
2020-08-05 18:02:35 +0200

    Can add in ccart

--- front/src/components/ShippingCart/ShippingCart.js
+++ front/src/components/ShippingCart/ShippingCart.js
@@ -1,25 +1,17 @@
 import React, {useEffect} from "react";
-import { articles } from "../../_actions/articles_actions";
 import {useDispatch, useSelector} from "react-redux";
-import {addToCart} from "../../_actions/shipping_cart_action";
 
 
 function Cart() {
     const dispatch = useDispatch();
-    const Articles = useSelector((state) => state.articles);
-
-    useEffect(() => {
-        articles().then((data) => dispatch(data));
-    }, [dispatch]);
-
-
+    const cart = useSelector((state) => state.shippingCart);
 
     return (
         <div>
             <h1> Articles_show </h1>
-            {Articles.list?.['hydra:member'].map(Article => (
-                <div key={Article.id}>
-                    {Article.name}<br/>
+            {cart.products.map(item => (
+                <div key={item.id}>
+                    {item.name}<br/>
                 </div>
             ))}
         </div>

9f2a0f20c06c69567c5c49274db99356b8ad93d8
Zach <pro.bizach@gmail.com>
2020-08-06 13:17:20 +0200

    A little view of the articles show page, with some gsap animation

--- front/src/components/Articles/Articles.js
+++ front/src/components/Articles/Articles.js
@@ -1,12 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { getArticles } from "../../_actions/articles_actions";
 import {addToCart} from "../../_actions/shipping_cart_action";
+import { TimelineMax, Power1, Bounce } from "gsap";
 
 const ArticleElement = ({ article }) => {
+  const articleRef = useRef(null);
+  const history = useHistory();
+
+  const handleMouseEnter = ({ target }) => {
+    const tl = new TimelineMax();
+    tl.to(
+      articleRef.current,
+      0.25,
+      {
+        backgroundColor: "#F8C027",
+        filter: "sepia(.5)",
+        yPercent: 10,
+        ease: Power1.easeOut,
+      },
+      0,
+    );
+  };
+  const handleMouseLeave = ({ target }) => {
+    const tl = new TimelineMax();
+
+    tl.to(articleRef.current, 0.75, {
+      backgroundColor: "initial",
+      filter: "grayscale(1)",
+      yPercent: 0,
+      ease: Bounce.easeOut,
+    });
+  };
+
+  const handleClick = () => {
+    history.push(`/articles/${article.id}`);
+  };
   return (
-    <div className="article-selector">
+    <div
+      className="article-selector"
+      ref={articleRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    >
       <img
         className="article-image"
         src={article.images}
@@ -16,7 +54,7 @@ const ArticleElement = ({ article }) => {
         <h5 className="article-name">{article.name}</h5>
         <span className="price"> &euro; {article.price} </span>
       </div>
-      <Link to={`/articles/${article.id}`}>Voir</Link>
+      {/* <Link to={`/articles/${article.id}`}>Voir</Link> */}
     </div>
   );
 };
@@ -29,6 +67,7 @@ function ArticlesShow() {
     getArticles().then((data) => dispatch(data));
   }, [dispatch]);
 
+<<<<<<< HEAD
     const handleAddCart = (e, article) => {
         e.preventDefault();
         dispatch(addToCart({article}));
@@ -49,6 +88,18 @@ function ArticlesShow() {
             ))}
         </div>
     )
+=======
+  return (
+    <div className="articles-container">
+      <h1> Tous les produits </h1>
+      <div className="articles">
+        {articles.list["hydra:member"]?.map((article) => (
+          <ArticleElement key={article.id} article={article} />
+        ))}
+      </div>
+    </div>
+  );
+>>>>>>> A little view of the articles show page, with some gsap animation
 }
 
 export default ArticlesShow;

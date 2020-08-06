53046866ef9095884fdd14ae9ee9133eeb2541fb
Jonas <jonas.flacher@epitech.eu>
2020-08-06 19:46:55 +0200

    Create API PLATEFORM ADMIN

---
+++ admin/src/authProvider.js
@@ -0,0 +1,37 @@
+const authProvider = {
+    login: ({ username, password }) =>  {
+        const request = new Request('http://localhost:8000/authentication_token', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                localStorage.setItem('token', token);
+            });
+    },
+    logout: () => {
+        localStorage.removeItem('token');
+        return Promise.resolve();
+    },
+    checkError: (error) => {
+        const status = error.status;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('token');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    checkAuth: () => localStorage.getItem('token')
+        ? Promise.resolve()
+        : Promise.reject({ redirectTo: '/no-access' }),
+    getPermissions: params => Promise.resolve(),
+};
+
+export default authProvider;

d104c7a318363615f9c5c889c31ff2a96b91e47b
Priscilla Joly <priscilla.joly@epitech.eu>
2020-08-23 11:40:44 +0200

    deploy admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,14 +3,19 @@ import { Redirect, Route } from "react-router-dom";
 import { HydraAdmin, hydraDataProvider as baseHydraDataProvider, fetchHydra as baseFetchHydra } from "@api-platform/admin";
 import parseHydraDocumentation from "@api-platform/api-doc-parser/lib/hydra/parseHydraDocumentation";
 import authProvider from "./authProvider";
+import {SERVER_URI_API} from "./config"
 
-const entrypoint = 'http://localhost:8000/api';
-const fetchHeaders = window.localStorage.getItem("token") ? { Authorization: `Bearer ${window.localStorage.getItem("token")}` } : {};
-const fetchHydra = (url, options = {}) => baseFetchHydra(url, {
-  ...options,
-  headers: new Headers(fetchHeaders),
-});
-const apiDocumentationParser = entrypoint => parseHydraDocumentation(entrypoint, { headers: new Headers(fetchHeaders) })
+const entrypoint = SERVER_URI_API;
+const fetchHeaders = () => window.localStorage.getItem("token") ? { Authorization: `Bearer ${window.localStorage.getItem("token")}` } : {};
+
+const fetchHydra = (url, options = {}) => {
+  return baseFetchHydra(url, {
+    ...options,
+    headers: new Headers(fetchHeaders()),
+  });
+};
+
+const apiDocumentationParser = entrypoint => parseHydraDocumentation(entrypoint, { headers: new Headers(fetchHeaders()) })
   .then(
     ({ api }) => ({ api }),
     (result) => {
---
+++ admin/src/config.js
@@ -0,0 +1,3 @@
+export const SERVER_URI = process.env.REACT_APP_API_URL;
+export const SERVER_URI_API = `${SERVER_URI}/api`;
+export const AUTHENTICATION_SERVER = `${SERVER_URI}/authentication_token`;

85ef89845a57bcbc522623c174c99a69b7b508bf
Priscilla Joly <priscilla.joly@epitech.eu>
2020-08-23 15:12:36 +0200

    add profile page

--- front/src/components/profile/profile.js
+++ front/src/components/profile/profile.js
@@ -1,34 +1,37 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+
 import {getProfile} from "../../_actions/user_actions";
-import articles_reducer from "../../_reducers/articles_reducer";
 
 function DisplayProfile() {
-        const dispatch = useDispatch();
-        //hook
-        const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
 
-        useEffect(() => {
-            getProfile().then((data) => dispatch(data));
-        }, [dispatch]);
+    useEffect(() => {
+        getProfile().then((data) => dispatch(data));
+    }, [dispatch]);
 
+    if (!user.loginSucces) {
+        return <Redirect
+            to={{
+                pathname: "/registry",
+            }}
+        />
+    }
 
-
-    
     return(
         <div>
-                <h1> Show the profile  </h1>
-                <div key={user.profile.id}>
-                        {  user.profile.name}<br/>
-                        {user.profile.surname}<br/>
-                        {user.profile.email}<br/>
-                        <Link to={`/profile/${user.profile.id}`}>après mes identifiant</Link>
-                    <hr/>
+            <h1>Profil</h1>
+            {user?.profile?.id && (
+                <div>
+                    {user.profile.username}<br/>
+                    {user.profile.name}<br/>
+                    {user.profile.surname}<br/>
+                    {user.profile.email}<br/>
+                    {user.profile.phone}<br/>
                 </div>
-
-
-
+            )}
         </div>
     )
 }
